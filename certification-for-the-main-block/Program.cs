1e4f5dca0d465c7eb1d52730bea4d00b87064d87
Olga <jilliya13@gmmail.com>
2024-05-12 08:12:51 +0300

    added repository for final work

---
+++ certification-for-the-main-block/Program.cs
@@ -0,0 +1,40 @@
+﻿void PrintWordsArray(string[] array)
+{
+    foreach (string elem in array)
+    {
+        Console.Write(elem + " ");
+    }
+}
+
+string[] CheckWordLenthInArray(string[] array)
+{
+    string[] result = new string[0];
+    int index = 0;
+    foreach (string item in array)
+    {
+        if (item.Length <= 3)
+        {
+            Array.Resize(ref result, result.Length + 1);
+            result[index] = item;
+            index++;
+        }
+    }
+    return result;
+}
+
+Console.WriteLine("So, first of all we should create an array with words.");
+Console.Write("How many words do you want to enter?: ");
+
+int wordsAmount = Convert.ToInt32(Console.ReadLine());
+string[] wordsArray = new string[wordsAmount];
+Console.WriteLine("Please, enter your words one by one.");
+
+for (int i = 0; i < wordsAmount; i++)
+{
+    Console.Write($"Enter word {i + 1}: ");
+    wordsArray[i] = Console.ReadLine();
+}
+
+string[] resultArray = CheckWordLenthInArray(wordsArray);
+
+PrintWordsArray(resultArray);
